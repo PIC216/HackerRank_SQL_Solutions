651c7adacb14fa703b5240c979ef743c1031aee9
Phoebe <101755626+PIC216@users.noreply.github.com>
2022-04-11 21:12:23 +0100

    Create Weather_Observation_Station_8.sql

---
+++ Basic/Basic SELECT/Weather_Observation_Station_8.sql
@@ -0,0 +1,14 @@
+SELECT city
+FROM station
+WHERE (city LIKE 'A%'
+OR city LIKE 'E%'
+OR city LIKE 'I%'
+OR city LIKE 'O%'
+OR city LIKE 'U%')
+AND
+(city LIKE '%a'
+OR city LIKE '%e'
+OR city LIKE '%i'
+OR city LIKE '%o'
+OR city LIKE '%u')
+;
