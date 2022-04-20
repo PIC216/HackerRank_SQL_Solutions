ff9aceb6460450229765d219ee6efa0e43485a78
Phoebe <101755626+PIC216@users.noreply.github.com>
2022-04-20 22:27:12 +0100

    Create Type_Of_Triangle.sql

---
+++ Basic/Advanced Select/Type_Of_Triangle.sql
@@ -0,0 +1,5 @@
+SELECT CASE WHEN A = B AND B = C THEN "Equilateral"
+            WHEN A+B <= C OR B+C <= A OR A+C <= B THEN "Not A Triangle"
+            WHEN A != B AND B != C AND A != C THEN "Scalene"
+            ELSE "Isosceles" END AS output
+FROM triangles
